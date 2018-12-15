4b65909e8ee6e850a48ac8720fdb7a0dbd046ed3
Philipin Pierre <etu25417@henallux.be>
2018-12-15 22:17:42 +0100

    ProviderController whisky

--- src/main/java/com/whisky/henallux/whisky/dataAccess/util/ProviderConverter.java
+++ src/main/java/com/whisky/henallux/whisky/dataAccess/util/ProviderConverter.java
@@ -1,4 +1,6 @@
 package com.whisky.henallux.whisky.dataAccess.util;
+import com.whisky.henallux.whisky.dataAccess.entity.WhiskyEntity;
+import com.whisky.henallux.whisky.model.Whisky;
 
 public class ProviderConverter {
 
@@ -13,4 +15,45 @@ public class ProviderConverter {
     {
         ;
     }*/
+
+    public WhiskyEntity WhiskyToWhiskyEntity(Whisky whisky){
+        WhiskyEntity whiskyEntity = new WhiskyEntity();
+        whiskyEntity.setAge(whisky.getAge());
+        whiskyEntity.setAlcoholContent(whisky.getAlcoholContent());
+        whiskyEntity.setBrand(whisky.getBrand());
+        whiskyEntity.setCategorie(whisky.getCategorie());
+        whiskyEntity.setCountry(whisky.getCountry());
+        whiskyEntity.setWhiskyName(whisky.getWhiskyName());
+        whiskyEntity.setId(whisky.getId());
+        whiskyEntity.setImg(whisky.getImg());
+        whiskyEntity.setPrice(whisky.getPrice());
+        whiskyEntity.setProductionDate(whisky.getProductionDate());
+        whiskyEntity.setPromotion(whisky.getPromotion());
+        whiskyEntity.setRegion(whisky.getRegion());
+        whiskyEntity.setSelection(whisky.getSelection());
+        whiskyEntity.setStockQuantity(whisky.getStockQuantity());
+        whiskyEntity.setVolume(whisky.getVolume());
+        return whiskyEntity;
+    }
+
+    public Whisky WhiskyEntityToWhisky(WhiskyEntity whiskyEntity){
+        Whisky whisky = new Whisky();
+        whisky.setBrand(whiskyEntity.getBrand());
+        whisky.setAge(whiskyEntity.getAge());
+        whisky.setAlcoholContent(whiskyEntity.getAlcoholContent());
+        whisky.setCategorie(whiskyEntity.getCategorie());
+        whisky.setCountry(whiskyEntity.getCountry());
+        whisky.setId(whiskyEntity.getId());
+        whisky.setImg(whiskyEntity.getImg());
+        whisky.setPrice(whiskyEntity.getPrice());
+        whisky.setProductionDate(whiskyEntity.getProductionDate());
+        whisky.setPromotion(whiskyEntity.getPromotion());
+        whisky.setCategorie(whiskyEntity.getCategorie());
+        whisky.setRegion(whiskyEntity.getRegion());
+        whisky.setStockQuantity(whiskyEntity.getStockQuantity());
+        whisky.setSelection(whiskyEntity.getSelection());
+        whisky.setVolume(whiskyEntity.getVolume());
+        whisky.setWhiskyName(whiskyEntity.getWhiskyName());
+        return whisky;
+    }
 }
