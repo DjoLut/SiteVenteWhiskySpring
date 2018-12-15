127dd7e2dfbfff39901f2af05e030d8997353025
Philipin Pierre <etu25417@henallux.be>
2018-12-15 19:05:24 +0100

    Base de données (2/2)

--- src/main/java/com/whisky/henallux/whisky/dataAccess/dao/TranslationDAO.java
+++ src/main/java/com/whisky/henallux/whisky/dataAccess/dao/TranslationDAO.java
@@ -1,7 +1,5 @@
 package com.whisky.henallux.whisky.dataAccess.dao;
 
-import com.whisky.henallux.whisky.dataAccess.entity.TranslationEntity;
-import com.whisky.henallux.whisky.dataAccess.entity.TranslationKey;
 import com.whisky.henallux.whisky.dataAccess.repository.TranslationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
