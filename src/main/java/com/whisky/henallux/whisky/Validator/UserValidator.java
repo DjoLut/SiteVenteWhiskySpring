40863cbf6b5790ca18a3f96aaf8a84e51e3ebddf
Philipin Pierre <etu25417@henallux.be>
2018-12-26 18:53:55 +0100

    Error register OK

--- src/main/java/com/whisky/henallux/whisky/Validator/UserValidator.java
+++ src/main/java/com/whisky/henallux/whisky/Validator/UserValidator.java
@@ -23,33 +23,33 @@ public class UserValidator implements Validator {
     public void validate(Object obj, Errors errors){
         User user = (User)obj;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"firstname", "username");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"username", "Empty.username");
         if(user.getUsername().length()<3 || user.getUsername().length()>30)
             errors.rejectValue("username","Size.username");
         if(userService.UserExist(user.getUsername()))
             errors.rejectValue("username", "Duplicate.username");
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Empty.password");
         if(user.getPassword().length()<3 || user.getPassword().length()>30)
             errors.rejectValue("password", "Size.password");
         if(!user.getPassword().equals(user.getConfPassword()))
             errors.rejectValue("confPassword", "Diff.confPassword");
 
-        ValidationUtils.rejectIfEmpty(errors,"firstname", "fistname");
+        ValidationUtils.rejectIfEmpty(errors,"firstname", "Empty.firstname");
         if(user.getFirstname().length()<3 || user.getFirstname().length()>50)
             errors.rejectValue("firstname", "Size.firstname");
 
-        ValidationUtils.rejectIfEmpty(errors,"lastname","lastname");
+        ValidationUtils.rejectIfEmpty(errors,"lastname","Empty.lastname");
         if(user.getLastname().length()<3 || user.getLastname().length() > 60)
             errors.rejectValue("lastname", "Size.lastname");
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"email","email");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"email","Empty.email");
         if(regex.equals(user.getEmail()))
             errors.rejectValue("email", "Invalid.email");
         if(userService.EmailExist(user.getEmail()))
             errors.rejectValue("email", "Duplicate.email");
 
-        ValidationUtils.rejectIfEmpty(errors,"adresse","adresse");
+        ValidationUtils.rejectIfEmpty(errors,"adresse","Empty.adresse");
         if(user.getAdresse().length()<15 || user.getAdresse().length() >200)
             errors.rejectValue("adresse", "Size.adresse");
 
--- src/main/webapp/WEB-INF/jsp/register.jsp
+++ src/main/webapp/WEB-INF/jsp/register.jsp
@@ -11,24 +11,24 @@
 					 <div>
 					 	<spring:bind path="username">
 							<span><form:label path="firstname"><spring:message code="firstname"/></form:label></span>
-							<form:input path="firstname" type="text"/>
 							<form:errors path="firstname"/>
+							<form:input path="firstname" type="text"/>
 					 	</spring:bind>
 					 </div>
 					 <div>
 						 <span><form:label path="lastname"><spring:message code="lastname"/></form:label></span>
-						<form:input path="lastname" type="text"/>
 						 <form:errors path="lastname"/>
+						<form:input path="lastname" type="text"/>
 					 </div>
 					 <div>
 						 <span><form:label path="email"><spring:message code="email"/></form:label></span>
-						 <form:input path="email" type="text"/>
 						 <form:errors path="email"/>
+						 <form:input path="email" type="text"/>
 					 </div>
 					 <div>
 						 <span><form:label path="adresse"><spring:message code="adresse"/></form:label></span>
-						 <form:input path="adresse" type="text"/>
 						 <form:errors path="adresse"/>
+						 <form:input path="adresse" type="text"/>
 					 </div>
 					 <div>
 						 <span><form:label path="numberTVA"><spring:message code="TVA"/></form:label></span>
@@ -40,18 +40,18 @@
 						    <h3><spring:message code="loginInformation"/></h3>
 							 <div>
 								 <span><form:label path="username"><spring:message code="login"/></form:label></span>
-								 <form:input path="username" type="text" />
 								 <form:errors path="username"/>
+								 <form:input path="username" type="text" />
 							 </div>
 							 <div>
 								 <span><form:label path="password"><spring:message code="password"/></form:label></span>
-								<form:input path="password" type="password"/>
 								 <form:errors path="password"/>
+								<form:input path="password" type="password"/>
 							 </div>
 							 <div>
 								 <span><form:label path="confPassword"><spring:message code="confPassword"/></form:label></span>
-								<form:input path="confPassword" type="password"/>
 								 <form:errors path="confPassword"/>
+								<form:input path="confPassword" type="password"/>
 							 </div>
 						 	<form:button id="login"><spring:message code="sendButton"/></form:button>
 					 </div>
