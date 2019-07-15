246d6dcf8ec7f2e9259a300e0b93026068435fad
Jordan L <etu32766@henallux.be>
2019-07-15 23:52:44 +0200

    Last push ?

---
+++ src/main/java/com/whisky/henallux/whisky/controller/ExceptionController.java
@@ -0,0 +1,15 @@
+package com.whisky.henallux.whisky.controller;
+
+import com.whisky.henallux.whisky.exception.AlreadyLoginException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(AlreadyLoginException.class)
+    public String AlreadyLogin(AlreadyLoginException ex)
+    {
+        return "redirect:/index";
+    }
+}
